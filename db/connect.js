1ee10c4bb7e40b1efda88ed4ba8578232e3276c7
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-04-17 14:59:16 -0300

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,19 +1,23 @@
+// /db/connect.js
 const { Sequelize } = require('sequelize');
 
-// Configuração do banco de dados PostgreSQL
 const sequelize = new Sequelize(process.env.PG_URI, {
     dialect: 'postgres',
-    logging: false, // Set to true for debugging SQL queries
+    logging: false,
 });
 
 async function connectDB() {
     try {
         await sequelize.authenticate();
-        console.log("✅ Conectado ao banco de dados PostgreSQL");
+        console.log("Conectado ao banco de dados PostgreSQL");
     } catch (err) {
         console.error("Erro ao conectar no banco:", err);
         process.exit(1);
     }
 }
 
-module.exports = connectDB;
+// Exporta tanto a instância quanto a função
+module.exports = {
+    sequelize,
+    connectDB
+};
