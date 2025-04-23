5c00337e23e916e9345f95150cf6c47ff876a126
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-04-23 12:02:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// index.js
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const webhookRoute = require('./routes/webhook');
-const connectDB = require('./db/connect');
+const { connectDB } = require('./db/connect'); // Conexão com o banco de dados
+const webhookRoute = require('./routes/webhook'); // Rota do webhook
 
 const app = express();
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json()); // Middleware para parsing de JSON
+
+// Definir a rota de webhook
 app.use('/webhook', webhookRoute);
 
-const PORT = process.env.PORT || 3000;
-connectDB().then(() => {
+// Conectar ao banco de dados e iniciar o servidor
+connectDB().then(async () => {
+    // Sincronizar o banco de dados
+    await sequelize.sync();
+
+    // Iniciar o servidor
     app.listen(PORT, () => {
         console.log(`🚀 Servidor rodando na porta ${PORT}`);
     });
