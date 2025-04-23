f4264f35f175885000c9bc8d9004b31d0267c03b
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-04-23 11:57:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const webhookRoute = require('./routes/webhook');
-const { connectDB, sequelize } = require('./db/connect');
+const connectDB = require('./db/connect');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middlewares
 app.use(bodyParser.json());
 
-// Rota para teste simples
-app.get('/', (req, res) => {
-    console.log('Requisição GET recebida!');
-    res.send('✅ Servidor está rodando!');
-});
-
-// Webhook do WhatsApp
 app.use('/webhook', webhookRoute);
 
-// Iniciar servidor
-(async () => {
-    try {
-        console.log('🔌 Conectando ao banco de dados...');
-        await connectDB();  // Conecta ao banco de dados
-        console.log('🔌 Banco de dados conectado com sucesso!');
-        
-        await sequelize.sync(); // Sincroniza o banco de dados
-        console.log('🔄 Banco de dados sincronizado!');
-        
-        app.listen(PORT, () => {
-            console.log(`🚀 Servidor rodando na porta ${PORT}`);
-        });
-    } catch (error) {
-        console.error('❌ Erro ao iniciar o servidor:', error);
-        process.exit(1);  // Encerra a aplicação caso falhe
-    }
-})();
+const PORT = process.env.PORT || 3000;
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`🚀 Servidor rodando na porta ${PORT}`);
+    });
+});
